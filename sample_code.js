12ff92401b2712399eaa227be00f437952f3b9e3
handiw <love901107>
2017-11-05 22:53:59 +0800

    js

---
+++ sample_code.js
@@ -0,0 +1,26 @@
+var name='xiaoming';
+var age=18;
+var height = 170 ,weight = 130.01;
+var isNan = true;
+var stuednt = false;
+var xinsheng_user_name = 'xiaoming';
+var wechatUserName ='xiaoming' ;
+var locationBase  = 'beijing';
+var company = locationBase + '新生大学';
+var myLikeColorList = ['blue','green','black','white'];
+var my_like_foods =['niuroumian','daoxiaomian','拉面','dunpling'];
+var mytime=myDate.toLocaleTimeString();
+//声明并定义变量
+var myJavaScriptLessonInfo ={
+    teacher:name,
+    teacher_wechat:wechatUserName,
+    title:'java入门技巧',
+    beginTime:'2018/11/5',
+    endTime:mytime,
+    boysStudents:417,
+    girlsStudent:419,
+    lessonTarget:'javascript 入门',
+    eat:my_like_foods,
+    color:myLikeColorList,
+    company:company
+};

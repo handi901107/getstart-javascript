0ea6e5bedae63223b4f15df2fae2a29fa59496f8
handiw <love901107>
2017-11-06 10:04:57 +0800

    zx

---
+++ sample.js
@@ -0,0 +1,19 @@
+function PersonA(){
+    console.log('1');
+    PersonB();
+}
+function PersonB(){
+    console.log('2');
+    personC();
+}
+function personC(){
+    console.log('3');
+    PersonD();
+}
+function PersonD(){
+    console.log('4');
+}
+function startSoundOFF(){
+    PersonA();
+}
+startSoundOFF();
